9507f12b4c4801a0f87f08c3ebc6a817b29efb50
msh <mshdevv@gmail.com>
2024-03-05 17:20:04 -0600

    Add basic product site

---
+++ boton.js
@@ -0,0 +1,9 @@
+// Selecciona el botón por su ID
+const boton = document.getElementById("card");
+
+// Agrega un evento de clic al botón
+boton.addEventListener("click", function() {
+  // Muestra una alerta cuando el botón se presiona
+  alert("El elemento fue añadido al carrito.\nQuedan 3 productos en stock.");
+});
+
