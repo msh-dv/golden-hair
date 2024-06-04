09d600ee763f393369fc05b6abb1e58aa33607a6
msh <mshdevv@gmail.com>
2024-06-04 15:35:07 -0600

    Update layout

--- js/menu-popup.js
+++ js/menu-popup.js
@@ -1,1 +1,21 @@
-const menu = getSelection("#popup-menu");
+const menu = document.getElementById("pop-menu");
+const showMenu = document.getElementById("show-pop");
+const closePopMenu = document.getElementById("close-pop-menu");
+
+let isMenuVisible = false;
+showMenu.addEventListener("click", () => {
+  console.log("here");
+  if (isMenuVisible) {
+    menu.style.opacity = "0";
+    menu.style.visibility = "hidden";
+  } else { menu.style.opacity = "1";
+    menu.style.visibility = "visible";
+  }
+  isMenuVisible = !isMenuVisible;
+});
+
+closePopMenu.addEventListener("click", () => {
+  menu.style.opacity = "0";
+  menu.style.visibility = "hidden";
+  isMenuVisible = !isMenuVisible;
+});
